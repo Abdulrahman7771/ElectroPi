0ca011b0b0dc462f651b34088e50dd753ab63471
unknown <ana.alandalib777@gmail.com>
2023-10-04 04:32:03 +0300

    asd

--- ComputerVision/IDExpert.py
+++ ComputerVision/IDExpert.py
@@ -1,41 +1,63 @@
-#import easyocr
 import pandas as pd
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
 import pytesseract
-pd.set_option('display.max_columns', None)
+import streamlit as st
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+if "file_uploader_key" not in st.session_state:
+    st.session_state["file_uploader_key"] = 0
 
-img = cv2.imread("ID.jpg")
-gray = cv2.imread("ID.jpg",0)
-rgb = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
+if "uploaded_files" not in st.session_state:
+    st.session_state["uploaded_files"] = []
 
-#reader = easyocr.Reader(['ar', 'en'],gpu=True)
-#result = reader.readtext(rgb)
+file = st.file_uploader(
+    "Upload your data file",
+    accept_multiple_files=False,
+    key=st.session_state["file_uploader_key"],
+)
 
-print(pytesseract.image_to_string(rgb,lang="eng"))
+if file:
+    st.session_state["uploaded_files"] = file
 
-#imgDataFrame = pd.DataFrame(result,columns=["Box","Text","Percentage"])
-#print(imgDataFrame)
+if file is not None:
+        img = cv2.imread(file.name)
+st.image(file)
+img = cv2.imread('ID.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+kernel = np.ones((2,2))
 
+
+ret, thresh = cv2.threshold(gray
+                            , 127, 255
+                            ,  cv2.THRESH_BINARY)
+
+
+laplacian = cv2.Laplacian(thresh, -1,ksize=7)
+OLaplacian =laplacian
+laplacian = laplacian[:laplacian.shape[0]-int(laplacian.shape[1]/6), int(laplacian.shape[1]/3):laplacian.shape[1]]
+
+
+rgb = cv2.cvtColor(laplacian, cv2.COLOR_BGR2RGB)
 plt.imshow(rgb)
+plt.show()
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+result = pytesseract.image_to_string(laplacian,lang="ara")
+print(result)
 
+laplacian = OLaplacian[OLaplacian.shape[0]-int(OLaplacian.shape[0]/3):OLaplacian.shape[0],int(OLaplacian.shape[1]/3):OLaplacian.shape[1]]
+rgb = cv2.cvtColor(laplacian, cv2.COLOR_BGR2RGB)
+plt.imshow(rgb)
+plt.show()
+numres = pytesseract.image_to_string(laplacian
+                                     ,lang="aranumberLayer2")
+print(numres)
 
-
-
-
-
-
-
-
-
-
-
-
-
+i=0
+for word in result.split("\n"):
+    if(word != ""):
+        print(i,": ",word)
+        i=i+1
 
 
 
